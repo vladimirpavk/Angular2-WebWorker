5f37d54c53fdaf40d3ba4e13c865af63794533ea
Vladimir Pavković <vladimirpavk@gmail.com>
2016-12-09 12:29:46 +0100

    Promise still not working corectly

--- app/ts/webworker/webworker.component.ts
+++ app/ts/webworker/webworker.component.ts
@@ -1,36 +1,42 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { JS_Worker } from './worker';
+import { WebWorkerService } from './webworker.service';
 @Component({
     selector: 'webworker',
-    templateUrl: './app/ts/webworker/webworker.component.html'
+    templateUrl: './app/ts/webworker/webworker.component.html',
+    providers: [ WebWorkerService ]    
 })
 export class WebWorkerComponent implements OnInit{
     @Input() count: number;      
     @Input() worker_name: string;
 
-    @Input() cur_value: number;
+    private cur_value: number = 5;
 
-    private mojaPcelica: JS_Worker;
+    constructor(private webWorkerService:WebWorkerService){}
 
     ngOnInit(){
-        console.log("init");
-        this.InitializeCurValue();
-        console.log(this.worker_name+" "+this.count+" "+this.cur_value);        
+        console.log("init");  
+        console.log(this.worker_name+" "+this.count+" "+this.cur_value);              
+
+        this.webWorkerService.returnPromise().subscribe((randomNum)=>{
+            this.cur_value++;
+            console.log('from returnPromise(): '+this.cur_value);
+        });
+
+         this.webWorkerService.getRandomNumbers(this.count);
+         
+         
+         
         
-        this.mojaPcelica=new JS_Worker('./app/js/webworker/jscript.js', this.myCallBack, 50);        
+        //this.webWorkerService.getRandomNumbers(this.count);
+       
+        //this.mojaPcelica=new JS_Worker('./app/js/webworker/jscript.js', this.myCallBack, 50);        
     }
-
-    private InitializeCurValue(){
-        this.cur_value=5;
-        console.log(this.cur_value);
-    }
-
+    
     public myCallBack(e):void
     {
         /*console.log(e.data);
         this.cur_value=this.cur_value+1;
         console.log("cur_value :"+this.cur_value);*/
-        //console.log(this.cur_value);
-        this.InitializeCurValue();
+        //console.log(this.cur_value);    
     }
 }
--- app/ts/webworker/webworker.service.ts
+++ app/ts/webworker/webworker.service.ts
@@ -3,19 +3,27 @@ import { Injectable } from '@angular/core';
 export class WebWorkerService{
     
     private webWorker: any;
+    private current_value: number = 0;
 
     constructor(){
+        //initialize and configure Web Worker
         this.webWorker=new Worker('./app/js/webworker/jscript.js');        
+        console.log("Service constructor...");
+        
+        this.webWorker.onmessage=((message_event_data)=>{            
+            console.log('From this.webWorker.onmessage '+message_event_data.data);
+            this.current_value=message_event_data.data;
+            this.returnPromise();
+        });
     }
 
-    public getRandomNumbers(count: number): Promise<number>{
+    public getRandomNumbers(count: number): any{                       
+        this.webWorker.postMessage(count);
+    }
 
-        this.webWorker.onmessage((random_num)=>{
-            return Promise.resolve(random_num);
-        });
-
-        this.webWorker.postmessage(count);
-
+    public returnPromise():Promise<number[]>{
+        console.log("From returnPromise :"+this.current_value);
+        return Promise.resolve<number[]>(this.current_value);
     }
 
 }
