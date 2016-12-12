94d194d7bc9b2a64dc61e5053f3ffb3bcb7c5074
Vladimir Pavković <vladimirpavk@gmail.com>
2016-12-12 15:39:53 +0100

    Everything seems to be ok but DOM does not refresh

--- app/ts/webworker/webworker.component.ts
+++ app/ts/webworker/webworker.component.ts
@@ -9,16 +9,26 @@ export class WebWorkerComponent implements OnInit{
     @Input() count: number;      
     @Input() worker_name: string;
 
-    private cur_value: number = 5;
+    public cur_value: number = 10;
 
-    constructor(private webWorkerService:WebWorkerService){}
+    constructor(private webWorkerService:WebWorkerService){        
+    }
+
 
     ngOnInit(){
         console.log("init");  
         console.log(this.worker_name+" "+this.count+" "+this.cur_value);                     
 
-        this.webWorkerService.numberGenerated.subscribe((value)=>console.log("From component :"+value));
+        //this.webWorkerService.numberGenerated.subscribe(this.numberGenerated);
+        this.webWorkerService.numberGenerated.subscribe((value)=>{
+            this.cur_value=value;
+            console.log("From cur_value: "+this.cur_value);
+        });
 
         this.webWorkerService.getRandomNumbers(this.count);
     }  
+
+    public numberGenerated(value:number){
+            this.cur_value=value;
+    }
 }
